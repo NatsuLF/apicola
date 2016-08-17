ad380bc2760225b5ea83d55d268c9843b0470ece
Leonel Barrera <leodota7@gmail.com>
2016-08-17 13:32:34 -0600

    New view

---
+++ resources/views/colmen.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section('title', 'La Colmena')
+
+@section('content')
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Panel title</h3>
+            </div>
+            <div class="panel-body">
+                Panel content
+            </div>
+        </div>
+    </div>
+@endsection
