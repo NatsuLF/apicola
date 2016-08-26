8aaf737f6aa9f19e9d293b584c8ccd4c3f4bd235
Mario Martinez <amakenadog@gmail.com>
2016-08-26 09:19:13 -0600

    Updated mission english translation

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -29,7 +29,7 @@ return [
 
     // mision vision
     'mission' => 'Mission',
-    'mission_content' => 'We are a company dedicated to research, innovation and development of apiculture products quality. We design, manufacture and market model hexagonal hives Vincent, equipment and accessories for beekeeping in order to meet the highest expectations of our customers. We are highly committed to environmental conservation, restoration of ecosystems, and social - economic development of beekeeping.',
+    'mission_content' => 'We are a program dedicated to research, innovate technology, and product development in the beekeeping sector. We design, manufacture hexagonal Vincent hives based on the natural habitat of bees. We are highly committed to environmental conservation, restoration of ecosystems, and social - economic development of beekeeping promoting a sustainable production model.',
     'view' => 'View',
     'view_content' => 'Becoming a leader in the manufacture of hives and distribution and customer service are always looking for excellence and leadership in beekeeping.',
 
